fe2df23ef7e2f44e257c9b03996071bb92c04bed
V4vern <28131050+V4Vern@users.noreply.github.com>
2024-04-01 18:00:59 +0800

    Add javadocs for Activity.java

--- src/main/java/byteceps/activities/Activity.java
+++ src/main/java/byteceps/activities/Activity.java
@@ -1,21 +1,47 @@
 package byteceps.activities;
 
+/**
+ * Represents an activity in the byteceps application.
+ * Each Activity object has a name.
+ */
 public class Activity {
     protected String activityName;
 
+    /**
+     * Constructs a new Activity object with the specified activity name.
+     *
+     * @param activityName The name of the activity.
+     */
     public Activity(String activityName) {
         this.activityName = activityName;
     }
 
+    /**
+     * Returns the name of the activity.
+     *
+     * @return The name of the activity.
+     */
     public String getActivityName() {
         return activityName;
     }
 
+    /**
+     * Computes a hash code for the Activity object based on its name.
+     *
+     * @return The hash code value for this Activity.
+     */
     @Override
     public int hashCode() {
         return activityName.hashCode();
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * Two Activity objects are considered equal if they have the same name.
+     *
+     * @param obj The reference object with which to compare.
+     * @return true if this Activity is the same as the obj argument; false otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
