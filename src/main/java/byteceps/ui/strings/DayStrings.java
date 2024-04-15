8e448a2c6c9be8da40a36883cecbc5b9f2a3dc06
joshualeejunyi <joshua.lee.99@hotmail.com>
2024-04-15 14:41:40 +0800

    Remove unused strings

--- src/main/java/byteceps/ui/strings/DayStrings.java
+++ src/main/java/byteceps/ui/strings/DayStrings.java
@@ -18,11 +18,8 @@ public class DayStrings {
     public static final String DAY_SAT = "sat";
     public static final String DAY_SUNDAY = "sunday";
     public static final String DAY_SUN = "sun";
-    public static final String INVALID_DATE = "Not a valid date";
     public static final String FUTURE_DATE = "future dates are not allowed";
     public static final String INVALID_DAY = "Not a valid day";
     public static final String REST_DAY = "Rest day";
     public static final String YEAR_FORMAT = "yyyy-M-dd";
-    public static final String INVALID_DATE_FORMAT = "The date must be in the format yyyy-mm-dd!";
-
 }
